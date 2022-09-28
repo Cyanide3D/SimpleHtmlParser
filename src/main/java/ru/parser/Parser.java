e1a2ac5b3515a7c10f42ad39da77683bad9fc278
Cyanide3D <gamer1997235@yandex.ru>
2022-09-28 13:29:31 +0300

    +read html as array of lexemes

--- src/main/java/ru/parser/Parser.java
+++ src/main/java/ru/parser/Parser.java
@@ -1,9 +1,36 @@
 package ru.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PrimitiveIterator;
+
 public class Parser {
 
     public Node parse(String html) {
+        List<Lexeme> lexemes = readLexemesFromHtml(html);
         return null;
+    }
+
+    private List<Lexeme> readLexemesFromHtml(String html) {
+        List<Lexeme> lexemes = new ArrayList<>();
+
+        for (int i = 0; i < html.length(); i++) {
+            char c = html.charAt(i);
+            lexemes.add(new Lexeme(c, getLexemeType(c)));
+        }
+
+        return lexemes;
+    }
+
+    private LexemeType getLexemeType(Character character) {
+        return switch (character) {
+            case '<' -> LexemeType.LEFT_BRACER;
+            case '>' -> LexemeType.RIGHT_BRACER;
+            case '=' -> LexemeType.EQUAL;
+            case '"' -> LexemeType.QUOTATION_MARK;
+            case ' ' -> LexemeType.SPACE;
+            default -> LexemeType.CHARACTER;
+        };
     }
 
 }
